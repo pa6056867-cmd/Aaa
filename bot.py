65f296fa73032fa83b182559c0df2206a730d024
pa6056867-cmd <pa6056867@gmail.com>
2026-07-28 18:29:35 +0330

    add main bot file
    Commit changes

---
+++ bot.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+
+
+logging.basicConfig(
+    level=logging.INFO
+)
+
+
+async def main():
+
+    bot = Bot(
+        token=BOT_TOKEN
+    )
+
+    dp = Dispatcher()
+
+
+    print("🤖 کافی‌نت آنلاین فعال شد")
+
+
+    await dp.start_polling(
+        bot
+    )
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
