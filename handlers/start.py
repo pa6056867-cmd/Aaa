0ab8d32090d5faa1da11f70eb354d09c1e296777
pa6056867-cmd <pa6056867@gmail.com>
2026-07-28 22:13:17 +0330

    add start handler
    Add start command handler to respond to user.

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,20 @@
+cafinet-online-bot
+│
+├── bot.py
+├── config.py
+├── requirements.txt
+└── handlers
+    └── start.py
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
 
+router = Router()
+
+@router.message(Command("start"))
+async def start(message: Message):
+    await message.answer(
+        "🌹 سلام!\n"
+        "به ربات «کافی‌نت آنلاین» خوش اومدی.\n\n"
+        "برای استفاده از خدمات، از منوی ربات استفاده کن."
+    )
