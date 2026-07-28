405bf937e220308ad3ed7c0105c9277ec6af4799
pa6056867-cmd <pa6056867@gmail.com>
2026-07-28 22:57:29 +0330

    add services handler

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 
 from handlers.start import router as start_router
-
+from handlers.services import router as services_router
 logging.basicConfig(
     level=logging.INFO
 )
 
-
+dp.include_router(services_router)
 async def main():
 
     bot = Bot(
