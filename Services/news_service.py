fd4a59c805047aefdab60004505bb1d432792498
pa6056867-cmd <pa6056867@gmail.com>
2026-07-29 13:06:55 +0330

    Update news_service.py

--- Services/news_service.py
+++ Services/news_service.py
@@ -1,1 +1,22 @@
+import feedparser
 
+
+RSS_URL = "https://www.zoomit.ir/feed/"
+
+
+def get_news():
+
+    feed = feedparser.parse(RSS_URL)
+
+    news = []
+
+    for item in feed.entries[:5]:
+
+        news.append(
+            {
+                "title": item.title,
+                "link": item.link
+            }
+        )
+
+    return news
