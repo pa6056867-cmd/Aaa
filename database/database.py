5bf2bd398942a2d4019b034e8e07c58c1f9d38c1
pa6056867-cmd <pa6056867@gmail.com>
2026-07-29 12:35:16 +0330

    Update database.py

--- database/database.py
+++ database/database.py
@@ -21,3 +21,40 @@ def init_db():
 
     conn.commit()
     conn.close()
+import sqlite3
+
+
+DB_NAME = "cafinet.db"
+
+
+def init_db():
+
+    conn = sqlite3.connect(DB_NAME)
+
+    cursor = conn.cursor()
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS orders (
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        full_name TEXT,
+
+        phone TEXT,
+
+        service TEXT,
+
+        description TEXT,
+
+        status TEXT DEFAULT 'در انتظار',
+
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+
+    )
+    """)
+
+
+    conn.commit()
+
+    conn.close()
