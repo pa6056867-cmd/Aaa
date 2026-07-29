c1fb2265733979cb2179b7a2eb06988eef075188
pa6056867-cmd <pa6056867@gmail.com>
2026-07-29 14:02:08 +0330

    Update crud.py

--- database/crud.py
+++ database/crud.py
@@ -66,3 +66,19 @@ def get_stats():
 
 
     return orders, users, income or 0
+def get_all_users():
+
+    conn = sqlite3.connect(DB_NAME)
+
+    cur = conn.cursor()
+
+    cur.execute("""
+    SELECT DISTINCT user_id
+    FROM orders
+    """)
+
+    users = cur.fetchall()
+
+    conn.close()
+
+    return users
