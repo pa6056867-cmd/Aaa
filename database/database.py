535b0cd567c344a33cfb9834b23a6ae6f7b7bf3c
pa6056867-cmd <pa6056867@gmail.com>
2026-07-29 11:50:01 +0330

    Create database.py

---
+++ database/database.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+DB_NAME = "cafinet.db"
+
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS orders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        full_name TEXT NOT NULL,
+        phone TEXT NOT NULL,
+        service TEXT NOT NULL,
+        description TEXT,
+        status TEXT DEFAULT 'در انتظار',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    conn.commit()
+    conn.close()
